1bee4798833f1d5a31ba51a252587f3bd5aa7795
alumno <alumno>
2019-02-08 12:26:38 -0300

    hacer ejercicio 9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,33 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+	var Sueldo;
+	var SueldoFinal;
+
+	Sueldo= sueldo.value;
 	
+	Sueldo= parseInt(Sueldo);
+
+	Sueldo= *0.10
+
+
+
+	document.getElementById('resultado').value=SueldoFinal;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
