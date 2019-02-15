0e37b55846375ff0cd367e85ab8019c5f6919f4c
alumno <alumno>
2019-02-15 12:19:18 -0300

    HACER EJERCICIOS IF

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,35 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+var numero;
+var mensaje;
+var nota;
+	numero= Math.floor((Math.random() * 10) + 1);
+
+	console.log (numero);
+
+		if (numero>8) {
+			mensaje="EXCELENTE";
+			}
+			if (numero>4) {
+				mensaje= "APROBO";
+				}
+				else (numero <5);
+				mensaje= "Vamos, la proxima se puede";
+
+
+
+
+
+
+				alert(mensaje);
+
+
+
+
+
+
+
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+		
+	var numero
+
+	numero= Math.floor((Math.random() * 10) + 1);
+
+	console.log (numero);
+
+
+
+
+
+
 
 }//FIN DE LA FUNCIÓN
